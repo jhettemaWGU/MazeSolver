5e4fde895e27f16a486cf51358cb96466621737b
Jonathan Hettema <jhettem@wgu.edu>
2024-08-21 16:20:43 -0700

    Added a visual representation of each step as it solves the maze.

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -7,21 +7,24 @@ public class MazeSolver {
     static int[][] maze = {
             {1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0},
             {0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0},
-            {0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1},
-            {1, 1, 1, 2, 0, 1, 1, 1, 0, 1, 0},
-            {0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0},
-            {0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1}
+            {0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0},
+            {1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
+            {0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0},
+            {0, 1, 0, 1, 0, 1, 3, 0, 2, 1, 1}
     };
+
+
 
     static LinkedList<Position> path = new LinkedList<Position>();
 
     public static void main(String[] args) {
-        Position p = new Position(4, 8);
+        Position p = createStartPosition();
         path.push(p);
 
         while (true) {
             int y = path.peek().y;
             int x = path.peek().x;
+            printMazeWithPath();
             maze[y][x] = 0;
 
             if(isValid(y+1, x)) {
@@ -81,13 +84,50 @@ public class MazeSolver {
         }
     }
 
-    public static boolean isValid(int y, int x) {
-        if (    y < 0 ||
-                y >= maze.length ||
-                x < 0 ||
-                x >= maze[0].length) {
-            return false;
+    static Position createStartPosition() {
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[i].length; j++) {
+                if (maze[i][j] == 3) {
+                    return new Position(i, j);
+                }
+            }
         }
-        return true;
+        System.out.println("No Starting Position found");
+        return null;
+    }
+
+    public static boolean isValid(int y, int x) {
+        return y >= 0 &&
+                y < maze.length &&
+                x >= 0 &&
+                x < maze[0].length;
+    }
+
+    static void printMazeWithPath() {
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[i].length; j++) {
+                boolean onPath = false;
+                for (Position p : path) {
+                    if (p.y == i && p.x == j) {
+                        System.out.print("P ");
+                        onPath = true;
+                        break;
+                    }
+                }
+                if (!onPath) {
+                    if (maze[i][j] == 1) {
+                        System.out.print(". ");
+                    } else if (maze[i][j] == 0) {
+                        System.out.print("  ");
+                    } else if (maze[i][j] == 2) {
+                        System.out.print("E ");
+                    } else if (maze[i][j] == 3) {
+                        System.out.print("S ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
