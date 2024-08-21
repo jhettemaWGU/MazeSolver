bdc5d66e4f5cc068333ccea85f4691313d2af919
Jonathan Hettema <jhettem@wgu.edu>
2024-08-21 15:03:56 -0700

    Created MazeSolver.java and Position.java

--- src/MazeSolver.java
+++ src/MazeSolver.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class MazeSolver {
+
+
+import java.util.LinkedList;
+
+public class MazeSolver {
+
+    static int[][] maze = {
+            {2, 0, 1, 1},
+            {1, 1, 0, 1},
+            {0, 1, 1, 1}
+    };
+
+    static LinkedList<Position> path = new LinkedList<Position>();
+
+    public static void main(String[] args) {
+        Position p = new Position(3, 0);
+        path.push(p);
+
+        maze[path.peek().y][path.peek().x] = 0;
+
+    }
 }
